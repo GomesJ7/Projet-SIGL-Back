93929d2dde5071424222b67bf5a064f47553bdc1
GomesJ7 <98333692+GomesJ7@users.noreply.github.com>
2026-04-21 16:09:21 +0200

    first commit

---
+++ src/test/java/com/example/projet_sigl/MultiModuleLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_sigl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
